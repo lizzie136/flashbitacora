94cc90b2451ae64829bbe991e8e4a5d5dec29f2d
Elizabeth Portilla <lizzie136@Elizabeths-MacBook-Pro.local>
2017-06-16 15:55:57 -0500

    üpdate host

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-e strict';
+'use strict';
 
 const Hapi = require('hapi');
 
 // Create a server with a host and port
 const server = new Hapi.Server();
 server.connection({ 
-    host: 'https://test-app-laboratoria.herokuapp.com/', 
+    host: 'localhost', 
     port: 80 
 });
 
